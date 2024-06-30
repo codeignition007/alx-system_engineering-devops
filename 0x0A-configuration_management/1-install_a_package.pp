c67285f650740442604f5ca117c3d607a80d6cc8
Johnson Adeoye <johnsonadeoye888@gmail.com>
2024-06-29 19:58:33 -0700

    Y

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
-# Install puppet-lint
+# Installs flask, version 2.1.0.
 
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-# Ensure Flask 2.1.0 is installed via pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-  require => Package['python3-pip'],
-}
-
